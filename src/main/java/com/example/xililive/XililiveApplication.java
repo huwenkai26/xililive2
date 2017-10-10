8adec6505f7c386ec36e2941c7d1de9fc328c4f0
Administrator <1121948633@qq.com>
2017-10-10 10:07:48 +0800

    增加index接口

--- src/main/java/com/example/xililive/XililiveApplication.java
+++ src/main/java/com/example/xililive/XililiveApplication.java
@@ -1,11 +1,12 @@
 package com.example.xililive;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.boot.*;
+import org.springframework.boot.autoconfigure.*;
+import org.springframework.stereotype.*;
+import org.springframework.web.bind.annotation.*;
 
-@SpringBootApplication
+@Controller
+@EnableAutoConfiguration
 public class XililiveApplication {
 
 	@RequestMapping("/xililive")
@@ -14,21 +15,7 @@ public class XililiveApplication {
 		return MainActivity.domain();
 	}
 
-
-	@RequestMapping("/index")
-	@ResponseBody
-	String index(){
-		return "hello world";
-	}
-
-
-    @RequestMapping("/home")
-    @ResponseBody
-    String home2(){
-        return "hello home";
-    }
-
-	public static void main(String[] args) {
+	public static void main(String[] args) throws Exception {
 		SpringApplication.run(XililiveApplication.class, args);
 	}
 }
